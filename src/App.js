c7c6d9a9cba7ee1c87ecf961f7897e23e99c548b
GrantvanB <gvanbo@gmail.com>
2021-06-30 11:18:02 -0600

    app built in dev mode

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component'
 import './App.css';
+
 // import { render } from '@testing-library/react';
 
 class App extends Component {
   constructor() {
     super();
     this.state = { 
-      monsters: 
-      []};
+      monsters: [],
+      searchField: ''
+    };
   }
 
   componentDidMount() {
@@ -17,17 +20,26 @@ class App extends Component {
     .then(users => this.setState({ monsters: users }))
   }
 
-render() {
- return (
-    <div className="App">
-      < CardList name="Grant" > 
-      {
-        this.state.monsters.map(monster => <h1 key={ monster.id }> { monster.name } </h1>)
-      }
-      </ CardList>
-    </div>
-  );
-}
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value})
+  }; 
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase()));
+
+    return (
+      <div className="App">
+        <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder='search monsters'
+          handleChange={ this.handleChange } 
+        />
+        < CardList monsters={filteredMonsters} /> 
+      </div>
+    );
+  }   
 }
 
 export default App;
